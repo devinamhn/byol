89eb20584498fce7cd5e2352f0914b757e5024b3
Devina Mohan <devinamhn@gmail.com>
2025-03-09 19:52:55 +0000

    fix for loading finetuned weights from saved ckpt properly

--- byol/eval/encoding_rgz.py
+++ byol/eval/encoding_rgz.py
@@ -95,14 +95,23 @@ byol = BYOL.load_from_checkpoint(paths["main"] / "byol.ckpt")
 
 #load finetuned byol checkpoint
 # finetuned_ckpt_path = 'model_finetune'
-finetuned_ckpt_path = '/share/nas2/dmohan/byol_finetuning/byol/files/finetune/hbo9tznc/BYOL_finetune_reproduce/hbo9tznc/checkpoints/epoch=299-step=3600.ckpt'
+finetuned_ckpt_path = paths["main"] / "files/finetune/hbo9tznc/BYOL_finetune_reproduce/hbo9tznc/checkpoints/epoch=299-step=3600.ckpt"
 finetuned_ckpt = torch.load(finetuned_ckpt_path)
 
-# get state dict
-state_dict = finetuned_ckpt['state_dict'].keys()
+# get state dict keys
+# state_dict = finetuned_ckpt['state_dict'].keys()
 
+#get encoder and head parameters
 encoder_weights = {k: v for k, v in finetuned_ckpt["state_dict"].items() if k.startswith("encoder.")}
 head_weights = {k: v for k, v in finetuned_ckpt["state_dict"].items() if k.startswith("head.")}
+
+''' 
+rename encoder weights - imp for loading the finetuned parameters from the ckpt since
+finetuned model's state dict keys begin with 'encoder' and won't match with
+the state dict of a new FineTune model instance
+'''
+encoder_weights_renamed = {k.replace('encoder.', ''): v for k,v in encoder_weights.items()}
+
 # byol = FineTune.load_from_checkpoint(encoder_weights, head_weights)
 # byol = FineTune.load_from_checkpoint(finetuned_ckpt_path)
 
@@ -110,6 +119,7 @@ config_finetune = load_config_finetune()
 #create an object of the finetuning class
 head = 'linear' 
 config_finetune["finetune"]["seed"] = seed
+
 # model = FineTune(byol.encoder, head, dim = byol.encoder.dim, )
 model = FineTune(
     byol.encoder,
@@ -126,34 +136,17 @@ model = FineTune(
 #the model object should have updated layer names!!
 # print('from ckpt', (encoder_weights.keys()))
 
-encoder_weights_renamed = {k.replace('encoder.', ''): v for k,v in encoder_weights.items()}
 
 # print('new keys', encoder_weights_renamed.keys())
 
 
 # print('from model',(model.encoder.state_dict().keys()))
 
+#should also work if I directly use byol.encoder
 encoder = model.encoder
 encoder.load_state_dict(encoder_weights_renamed)
 # head.load_state_dict(head_weights)
 # head = model.head
-
-
-
-# SSL trained ckpt
-# config = byol.config
-# print(byol.encoder.dim)
-
-# byol.eval()
-# encoder = byol.encoder.cuda()
-# encoder.eval()
-
-# encoder.load_state_dict(finetuning_checkpoint)
-# print(len(encoder.state_dict().keys()))
-# print(encoder.state_dict().keys())
-# encoder = ResNet(block=BasicBlock, layers=[2, 2, 2, 2], n_c=1, downscale=True, features=512).cuda()
-# encoder.load_state_dict(torch.load("encoder.pt"))
-# encoder.eval()
 
 transform = T.Compose(
     [
